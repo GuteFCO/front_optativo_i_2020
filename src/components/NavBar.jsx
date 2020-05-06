3f653414b7b11a03a5f2d6db4f49a37786e374d5
Francisco Gutierrez <fgutierrez@grouponlatam.com>
2020-05-06 09:49:14 -0400

    Ejemplo clase

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+
+export default function NavBar() {
+  return (
+    <header>
+      <nav>
+        <ul>
+          <li><a href="register.html">Registrarse</a></li>
+          <li><a href="contact.html">Contacto</a></li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
